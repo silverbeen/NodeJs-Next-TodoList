40d7b2544b6c44a7f35ea195854077c12c1e82eb
강은빈 <kub0803@naver.com>
2021-12-08 23:45:36 +0900

    [ADD] redux-toolkit

--- client/createSlice/slices/index.ts
+++ client/createSlice/slices/index.ts
@@ -1,10 +1,21 @@
+import { HYDRATE } from "next-redux-wrapper";
 import { combineReducers } from "redux";
 import userSlice from "./user";
 
-const rootReduder = combineReducers({
-  users: userSlice.reducer,
-});
+const rootReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case HYDRATE: // 서버에서 생성된 상태를 클라이언트로 전달해 주는 역할
+      console.log("HYDRATE", action);
+      return action.payload;
+    default: {
+      const combineReducer = combineReducers({
+        user: userSlice.reducer,
+      });
+      return combineReducer(state, action);
+    }
+  }
+};
 
-export type RootState = ReturnType<typeof rootReduder>;
+export type RootState = ReturnType<typeof rootReducer>;
 
-export default rootReduder;
+export default rootReducer;
